a7e30681df47f15443313fd4be61e7541caab76d
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2022-07-21 22:07:35 -0700

    some exercise solutions
    untested

--- homeworks/learning_julia1_exercises.jl
+++ homeworks/learning_julia1_exercises.jl
@@ -32,34 +32,35 @@ using Test
 # Try the "gimmee" now!
 
 # Set `a` to be an array with two items, `1` and `7`
-a =
+a = [1,7]
 @test length(a) == 2 && a[1] == 1 && a[2] == 7
 
 # OK, now let's get serious!
 
 @testset "Learning Julia 1" begin
     # Create a range of all integers from 2 to 5, inclusive
-    r =
+    r = 2:5
     @test isa(r, AbstractUnitRange) && first(r) == 2 && last(r) == 5
 
     # Create an array that stores `Float64`s but otherwise holds the same values as `r` above
-    rf =
+    rf = Float64.(r)
     @test eltype(rf) == Float64 && rf == r
 
     # Create a matrix equal to a 2x2 identity matrix (1 on the diagonal, zero on the corners)
-    M =
+    M = [[1,0], [0,1]]
     @test size(M) == (2,2) && M[1,1] == M[2,2] == 1 && M[1,2] == M[2,1] == 0
 
     # Create a vector of `String`s that has 3 undefined elements, then make the middle one equal to "Julia"
-    vstr =
+    vstr = Vector{String}(undef, 3)
+    vstr[2] = "Julia"
     # you will need one more line here
     @test length(vstr) == 3 && eltype(vstr) == String && !isassigned(vstr, 1) && !isassigned(vstr, 3) && vstr[2] == "Julia"
 
     # Create two ranges, `row` and `col`: row should go from 1 to 3 (integers) and `col` should contain the even numbers between 2 and 8.
     # Use broadcasting to form a matrix of their product.
-    row =
-    col =
-    m =
+    row = 1:3
+    col = 2:2:8
+    m = row.*col'
     @test isa(row, AbstractUnitRange) && isa(col, AbstractRange) && first(row) == 1 && last(row) == 3 &&
           first(col) == 2 && last(col) == 8 && step(col) == 2
     @test m ==
@@ -69,18 +70,18 @@ a =
     ]
 
     # Create a tuple with a `String`, an `Int`, and a `Float64` (of your choice) in that order
-    t =
+    t = ("Tennis", 44, Float64(123.45))
     @test isa(t, Tuple) && isa(t[1], String) && isa(t[2], Int) && isa(t[3], Float64)
 
     # Create a NamedTuple with fields `make` and `model` with values "Honda" and "Odyssey", respectively
-    nt =
+    nt = (make="Honda", model="Odyssey")
     @test nt.make == "Honda"
     @test nt.model == "Odyssey"
     @test isa(nt, NamedTuple)
 
     # Split this string into words
     str = "Advanced scientific computing"
-    sstr =
+    sstr = split(str)
     @test length(sstr) == 3 && sstr[1] == "Advanced" && sstr[2] == "scientific" && sstr[3] == "computing"
 
     # Now things will start getting a bit (just a bit) harder
@@ -90,7 +91,9 @@ a =
     # Alphabetize the words in this string, omitting any that contain a 'u'
     # Do not type the answer directly, generate it by exploiting Julia's string-manipulation functions
     animals = "dog cat opossum feline antelope chimp octopus salamander"
-    aanimals =  # feel free to use multiple lines to solve this; `aanimals` should hold your final answer
+    # alphatize(animals) = join( sort( split(animals) ) , ' ')
+    alphatize(animals) = sort( split(animals) )
+    aanimals = alphatize(animals) # feel free to use multiple lines to solve this; `aanimals` should hold your final answer
     @test aanimals == ["antelope", "cat", "chimp", "dog", "feline", "salamander"]
 
     # Compute the sum of the ascii codes in `animals`, excluding spaces
